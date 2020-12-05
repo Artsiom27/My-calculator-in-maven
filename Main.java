2155f9a1f4fce5846aa805e6b9050beeafb9c656
Artsiom Tsvikevich <temancvik@gmail.com>
2020-12-05 21:26:38 +0300

    second commit

--- Main.java
+++ Main.java
@@ -1,50 +1,59 @@
 package com.company;
-
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-class Main {
+public class Main {
 
     public static void main(String[] args) {
 
         boolean e = false;
 
-        do {
+        try {
 
-            Scanner in = new Scanner(System.in);
-            System.out.print("Введите первое число: ");
-            int num1 = in.nextInt();
+            do {
 
-            System.out.print("Введите второе число: ");
-            int num2 = in.nextInt();
+                Scanner in = new Scanner(System.in);
+                System.out.print("Введите первое число: ");
+                int num1 = in.nextInt();
 
-            Scanner in2 = new Scanner(System.in);
-            System.out.print("Выберите операцию(+,-,/,*): ");
-            String num3 = in2.nextLine();
-            switch (num3) {
-                case "+":
-                    System.out.println("Результат: " + (num1 + num2));
-                    break;
-                case "-":
-                    System.out.println("Результат: " + (num1 + num2));
-                    break;
-                case "*":
-                    System.out.println("Результат: " + (num1 * num2));
-                    break;
-                case "/":
-                    System.out.println("Результат: " + (num1 / num2));
-                    break;
+                System.out.print("Введите второе число: ");
+                int num2 = in.nextInt();
+
+                Scanner in2 = new Scanner(System.in);
+                System.out.print("Выберите операцию(+,-,/,*): ");
+                String num3 = in2.nextLine();
+                switch (num3) {
+                    case "+":
+                        System.out.println("Результат: " + (num1 + num2));
+                        break;
+                    case "-":
+                        System.out.println("Результат: " + (num1 + num2));
+                        break;
+                    case "*":
+                        System.out.println("Результат: " + (num1 * num2));
+                        break;
+                    case "/":
+                        System.out.println("Результат: " + (num1 / num2));
+                        break;
+                    default:
+                        System.out.println("Неверная операция");
+                }
+                Scanner in0 = new Scanner(System.in);
+                System.out.print("Желаете еще посчитать(да или нет): ");
+                String c = in0.nextLine();
+                e = c.equals("нет");
             }
-            Scanner in0 = new Scanner(System.in);
-            System.out.print("Желаете еще посчитать(да или нет): ");
-            String c = in0.nextLine();
-            e = c.equals("нет");
+
+            while (!e);
+            System.out.println("Ну и УХОДИ");
 
         }
-        while (!e);
-        System.out.println("Ну и УХОДИ");
-
+            catch (InputMismatchException ex) { //я написал блок catch здесь, чтобы как ты и просил прога падала
+            System.out.println("Вы ввели строку"); // полностью при  вводе строки и выводила это сообщение
+        }
     }
 }
+
 
 
 
